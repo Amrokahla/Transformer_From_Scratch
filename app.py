7e1b670f6c5084e53806c27d42973d10af7b4ad2
Amro <amrokahla@yahoo.com>
2025-04-23 05:53:23 +0200

    app conflict

--- app.py
+++ app.py
@@ -13,14 +13,6 @@ nltk.download("words")
 # Build vocab from NLTK words
 nltk_vocab = ["<pad>", "<sos>", "<eos>"] + list(set(words.words()))
 vocab = {word: idx for idx, word in enumerate(nltk_vocab)}
-=======
-vocab = vocab = {
-    "<pad>": 0, "<sos>": 1, "<eos>": 2, "hello": 3, "world": 4,
-    "how": 5, "are": 6, "you": 7, "i": 8, "am": 9, "fine": 10,
-    "happy": 11, "the": 12, "sun": 13, "is": 14, "bright": 15,
-    "it": 16, "raining": 17, "weather": 18, "nice": 19
-}
->>>>>>> 026554b8cac52e4e11eea2860a7c9f86bb48f9b1
 
 tokenizer = SimpleTokenizer(vocab)
 
