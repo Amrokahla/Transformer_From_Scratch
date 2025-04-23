89291cbdc99c8af819e1c3a8520ac48c03f5f5d4
Amrokahla <85911760+Amrokahla@users.noreply.github.com>
2025-04-23 16:51:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from model.tokenizer import SimpleTokenizer
 
 nltk.download("words", quiet=True)
 
+# Build vocab
 nltk_vocab = ["<pad>", "<sos>", "<eos>", "<unk>"] + list(set(words.words()))
 vocab = {word: idx for idx, word in enumerate(nltk_vocab)}
 
@@ -19,17 +20,19 @@ def plot_attention(attn, input_tokens):
     sns.heatmap(attn, xticklabels=input_tokens, yticklabels=input_tokens, cmap="viridis")
     st.pyplot(fig)
 
+# Streamlit UI
 st.title("Transformer Attention Visualizer")
 st.subheader("What will the transformer pay attention to")
 
 text = st.text_input("Enter sentence:", "hello world")
 
-D_model = st.slider("Model dimension (d_model)", 64, 256, 128, step = 64)
-num_heads = st.slider("Number of Attention Heads", 4, 8, 4, step =4 )
+d_model = st.slider("Model dimension (d_model)", 64, 256, 128, step=64)
+num_heads = st.slider("Number of Attention Heads", 4, 8, 4, step=4)
 num_layers = st.slider("Number of Transformer Layers", 1, 6, 2)
 
 run = st.button("Run Transformer")
 
+# Session state initialization
 if "logits" not in st.session_state:
     st.session_state["logits"] = None
     st.session_state["attentions"] = None
@@ -42,7 +45,7 @@ if run:
         d_model=d_model,
         num_heads=num_heads,
         num_layers=num_layers,
-        ff_dim=4*d_model
+        ff_dim=4 * d_model
     )
     model.eval()
 
@@ -59,7 +62,7 @@ if run:
     st.session_state["attentions"] = attentions
     st.session_state["tokens"] = tokens
 
-if st.session_state["attentions"]:
+if st.session_state["attentions"] is not None:
     tokens = st.session_state["tokens"]
     attentions = st.session_state["attentions"]
 
@@ -69,7 +72,6 @@ if st.session_state["attentions"]:
 
     num_heads_available = attentions[0].shape[1]
     head = st.selectbox("Head", list(range(num_heads_available)), index=0, format_func=lambda x: f"Head {x}")
-
 
     attn = attentions[layer][0, head].detach().numpy()
     plot_attention(attn, tokens)
